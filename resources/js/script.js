9bb17d3a549613867d661cdaae69ce6c4e478e3a
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-02-10 12:56:45 +0100

    Added functions to set up game

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,18 @@
 var square = document.querySelectorAll(".square");
 var rgbColor = document.querySelectorAll(".rgbColor");
+var content = document.querySelector("#content");
 var squareColors = [];
 var easyGame = document.querySelector("#easy");
 var hardGame = document.querySelector("#hard");
 var newColors = document.querySelector("#newColors");
+var numberOfSquare = 6;
 
+setUpGame();
+
+function setUpGame() {
+    fillArrayWithColors();
+    addSquares(numberOfSquare);
+}
 
 /*ustawia randomowo nagłówek*/
 for (var i = 0; i < rgbColor.length; i++) {
@@ -35,4 +43,30 @@ function fillArrayWithColors() {
     }
 }
 
-fillArrayWithColors();
+/*ustawianie ilości square'ów w zależności do poziomu trudności gry*/
+easyGame.addEventListener("click", function() {
+    numberOfSquare = 3;
+    setUpGame();
+})
+
+hardGame.addEventListener("click", function() {
+    numberOfSquare = 6;
+    setUpGame();
+})
+
+/*generowanie square'ów w zależnosci od ustawionej ilości*/
+function addSquares(n) {
+    content.innerHTML = "";
+    for (var i = 0; i < n; i++) {
+        var square = document.createElement("div");
+        square.classList.add("square");
+        content.appendChild(square);
+    }
+}
+
+/*pobiera dane z obiektu zawierającego randomowe r, g i b i zamienia na string*/
+function createRGB(color) {
+    return "rgb(" + color.r + ", " + color.g + ", " + color.b + ")";
+}
+
+function 
